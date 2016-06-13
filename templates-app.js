eeb51a987cd310a0c709dc874bc2659d752709c0
Matthew Green <matthew@newedgeengineering.com>
2016-06-13 10:44:18 +0100

    tidy up based on reviewers feed back

--- templates-app.js
+++ templates-app.js
@@ -4,9 +4,18 @@ angular.module("about/about.tpl.html", []).run(["$templateCache", function($temp
   $templateCache.put("about/about.tpl.html",
     "<div class=\"row\">\n" +
     "    <h1 class=\"page-header\">Since 2009...</h1>\n" +
-    "    <p>...we have been providing technology consultancy, <a href=\"#/technologies\">solutions</a>, training and production to a variety of <a href=\"#/clients\">clients</a>. Helping them to bring to life their vision of a rich media experience. Tho we have not existing for very long, we do have over ten years of experience helping companies create their digital presence in many forms including interactive television, websites and mobile devices. We also believe in helping people to understand stand their technical options and the solutions that can be applied to create their digital presence that will allow our clients' customers to get the best the experience of our client's products and services.</p>\n" +
+    "</div>\n" +
+    "<div class=\"row\">\n" +
+    "    <div class=\"col-xs-12 col-sm-9 col-sm-offset-3 col-md-9 col-md-offset-3 col-lg-10 col-lg-offset-2 text-right\">\n" +
+    "        <h2>...we have been providing technology consultancy, solutions, training and production to a variety of clients.</h2>\n" +
+    "    </div>\n" +
+    "</div>\n" +
+    "    <p>Helping them to bring to life their vision of a rich media experience.</p>\n" +
+    "    <p>Our team have over many years of experience helping companies create their digital presence in many forms including interactive television, websites and for mobile devices.</p>\n" +
+    "    <p>We also believe in helping people to understand stand their technical options and the solutions that can be applied to create their digital presence. Which will allow consumers to get the best experience of our client's products and services.</p>\n" +
+    "    <br/>\n" +
     "    <blockquote>\n" +
-    "        <p>We work at the fore front of technology allowing us to make peoples dreams and visions become a reality.</p>\n" +
+    "        <p class=\"text-center\">We work at the fore front of technology allowing us to make peoples dreams and visions become a reality.</p>\n" +
     "    </blockquote>\n" +
     "</div>\n" +
     "\n" +
@@ -16,24 +25,28 @@ angular.module("about/about.tpl.html", []).run(["$templateCache", function($temp
 angular.module("clients/clients.tpl.html", []).run(["$templateCache", function($templateCache) {
   $templateCache.put("clients/clients.tpl.html",
     "<div class=\"row clients\">\n" +
-    "    <h1 class=\"page-header\">We Have Helped</h1>\n" +
+    "    <h1 class=\"page-header\">Who We Have Helped</h1>\n" +
     "    <div class=\"row text-center\">\n" +
-    "        <div class=\"col-sm-4\"><img src=\"clients/incopro.png\" width=\"100\" height=\"60\" title=\"Incopro\"/></div>\n" +
-    "        <div class=\"col-sm-4\"><img src=\"clients/blackwells.png\" width=\"60\" height=\"60\" title=\"Blackwells Learning\"/></div>\n" +
-    "        <div class=\"col-sm-4\"><img src=\"clients/orange-digital.jpg\" width=\"60\" height=\"60\" title=\"Orange Digital\"/></div>\n" +
+    "        <div class=\"hidden-xs hidden-sm col-md-1 col-ld-1\"></div>\n" +
+    "        <div class=\"col-xs-12 col-sm-6 col-md-2 col-ld-2\"><div class=\"frame\"> <img src=\"clients/incopro.png\" width=\"100\" height=\"60\" title=\"Incopro\"/></div></div>\n" +
+    "        <div class=\"col-xs-12 col-sm-6 col-md-2 col-ld-2\"><div class=\"frame\"><img src=\"clients/blackwells.png\" width=\"60\" height=\"60\" title=\"Blackwells Learning\"/></div></div>\n" +
+    "        <div class=\"col-xs-12 col-sm-6 col-md-2 col-ld-2\"><div class=\"frame\"><img src=\"clients/orange-digital.jpg\" width=\"60\" height=\"60\" title=\"Orange Digital\"/></div></div>\n" +
+    "        <div class=\"col-xs-12 col-sm-6 col-md-2 col-ld-2\"><div class=\"frame\"><img src=\"clients/momentum.png\" width=\"60\" height=\"60\" title=\"Momentum\"/></div></div>\n" +
+    "        <div class=\"col-xs-12 hidden-sm col-md-2 col-ld-2\"><div class=\"frame\"><img src=\"clients/imagination.png\" width=\"100\" height=\"24\" title=\"Imagination\"/></div></div>\n" +
+    "        <div class=\"hidden-xs hidden-sm col-md-1 col-ld-1\"></div>\n" +
+    "    </div>\n" +
+    "    <div class=\"row text-center visible-xs-block visible-sm-block\">\n" +
+    "        <div class=\"hidden-xs col-sm-6\"><div class=\"frame\"><img src=\"clients/imagination.png\" width=\"100\" height=\"24\" title=\"Imagination\"/></div></div>\n" +
+    "        <div class=\"hidden-xs col-sm-6\"><div class=\"frame\"><img src=\"clients/77pr.png\" width=\"64\" height=\"60\" title=\"SeventySeven PR\"/></div></div>\n" +
     "    </div>\n" +
     "    <div class=\"row text-center\">\n" +
-    "        <div class=\"col-sm-4\"><img src=\"clients/momentum.png\" width=\"60\" height=\"60\" title=\"Momentum\"/></div>\n" +
-    "        <div class=\"col-sm-4\"><img src=\"clients/imagination.png\" width=\"100\" height=\"24\" title=\"Imagination\"/></div>\n" +
-    "        <div class=\"col-sm-4\"><img src=\"clients/77pr.png\" width=\"64\" height=\"60\" title=\"SeventySeven PR\"/></div>\n" +
-    "    </div>\n" +
-    "    <div class=\"row text-center\">\n" +
-    "        <div class=\"col-sm-4\"><img src=\"clients/wazoku.png\" width=\"100\" height=\"60\" title=\"Wazoku\" /></div>\n" +
-    "        <div class=\"col-sm-4\"><img src=\"clients/streamingtank.png\" width=\"100\" height=\"38\" title=\"Streaming Tank\"/></div>\n" +
-    "        <div class=\"col-sm-4\"><img src=\"clients/sky.png\" width=\"100\" height=\"60\" title=\"Sky\" /></div>\n" +
-    "    </div>\n" +
-    "    <div class=\"row text-center\">\n" +
-    "        <div class=\"col-sm-12\"><img src=\"clients/nativ.png\" width=\"100\" height=\"46\" title=\"Nativ\" /></div>\n" +
+    "        <div class=\"hidden-xs hidden-sm col-md-1 col-ld-1\"></div>\n" +
+    "        <div class=\"col-xs-12 hidden-sm col-md-2 col-ld-2\"><div class=\"frame\"><img src=\"clients/77pr.png\" width=\"64\" height=\"60\" title=\"SeventySeven PR\"/></div></div>\n" +
+    "        <div class=\"col-xs-12 col-sm-6 col-md-2 col-ld-2\"><div class=\"frame\"><img src=\"clients/wazoku.png\" width=\"100\" height=\"60\" title=\"Wazoku\" /></div></div>\n" +
+    "        <div class=\"col-xs-12 col-sm-6 col-md-2 col-ld-2\"><div class=\"frame\"><img src=\"clients/streamingtank.png\" width=\"100\" height=\"38\" title=\"Streaming Tank\"/></div></div>\n" +
+    "        <div class=\"col-xs-12 col-sm-6 col-md-2 col-ld-2\"><div class=\"frame\"><img src=\"clients/sky.png\" width=\"100\" height=\"60\" title=\"Sky\" /></div></div>\n" +
+    "        <div class=\"col-xs-12 col-sm-6 col-md-2 col-ld-2\"><div class=\"frame\"><img src=\"clients/nativ.png\" width=\"100\" height=\"46\" title=\"Nativ\" /></div></div>\n" +
+    "        <div class=\"hidden-xs hidden-sm col-md-1 col-ld-1\"></div>\n" +
     "    </div>\n" +
     "</div>\n" +
     "\n" +
@@ -44,21 +57,37 @@ angular.module("contact/contact.tpl.html", []).run(["$templateCache", function($
   $templateCache.put("contact/contact.tpl.html",
     "<div class=\"row contact\">\n" +
     "    <h1 class=\"page-header\">How To Enquire</h1>\n" +
-    "    <p>If you have an enquiry, please don't hesitate to email us at <a href=\"mailto:enquiries@newedgeengineering.com\">enquiries@newedgeengineering.com</a></p>\n" +
-    "    <ul class=\"social hidden-md hidden-lg\">\n" +
-    "        <li>\n" +
-    "            <a target=\"_blank\" href=\"https://www.google.com/+NewedgeengineeringLimited\"><i class=\"fa fa-google-plus\" aria-hidden=\"true\"></i></a>\n" +
-    "        </li>\n" +
-    "        <li>\n" +
-    "            <a target=\"_blank\" href=\"http://twitter.com/newedgeengineer\"><i class=\"fa fa-twitter\"></i></a>\n" +
-    "        </li>\n" +
-    "        <li>\n" +
-    "            <a target=\"_blank\" href=\"https://www.linkedin.com/company/new-edge-engineering-ltd\"><i class=\"fa fa-linkedin\"></i></a>\n" +
-    "        </li>\n" +
-    "        <li>\n" +
-    "            <a target=\"_blank\" href=\"https://github.com/New-Edge-Engineering/\"><i class=\"fa fa-github\"></i></a>\n" +
-    "        </li>\n" +
-    "    </ul>\n" +
+    "    <div class=\"invert\" data-ng-show=\"capturing\">\n" +
+    "        <p>If you have an enquiry, please don't hesitate to email us at <a href=\"mailto:enquiries@newedgeengineering.com\">enquiries@newedgeengineering.com</a> or fill in the form.</p>\n" +
+    "        <!-- https://github.com/dwyl/html-form-send-email-via-google-script-without-server -->\n" +
+    "        <div class=\"alert alert-danger alert-dismissible\" role=\"alert\" data-ng-show=\"error\">\n" +
+    "            <button type=\"button\" class=\"close\" data-ng-click=\"close()\" aria-label=\"Close\"><span aria-hidden=\"true\">&times;</span></button>\n" +
+    "            <span><strong>A problem occurred,</strong> please try again later.</span>\n" +
+    "        </div>\n" +
+    "        <form>\n" +
+    "            <div class=\"form-group\">\n" +
+    "                <label for=\"name\">Name<span class=\"aui-icon icon-required\"></span></label>\n" +
+    "                <input class=\"form-control\" id=\"name\" name=\"name\" placeholder=\"Name\" required data-ng-model=\"enquiry.name\">\n" +
+    "            </div>\n" +
+    "            <div class=\"form-group\">\n" +
+    "                <label for=\"email\">Email Address<span class=\"aui-icon icon-required\"></span></label>\n" +
+    "                <input class=\"form-control\" id=\"email\" name=\"email\" type=\"email\" placeholder=\"j.doe@example.com\" required data-ng-model=\"enquiry.email\">\n" +
+    "            </div>\n" +
+    "            <div class=\"form-group\">\n" +
+    "                <label for=\"email\">Message<span class=\"aui-icon icon-required\"></span></label><br/>\n" +
+    "                <textarea id=\"message\" name=\"message\" rows=\"10\" placeholder=\"j.doe@example.com\" required data-ng-model=\"enquiry.message\"></textarea>\n" +
+    "            </div>\n" +
+    "            <div class=\"text-right\">\n" +
+    "                <button class=\"btn btn-danger btn-lg\" type=\"submit\" data-ng-disabled=\"contact.$valid == false\" data-ng-click=\"send()\"><i class=\"fa fa-paper-plane\"></i> Send</button>\n" +
+    "            </div>\n" +
+    "        </form>\n" +
+    "    </div>\n" +
+    "\n" +
+    "    <div class=\"text-center\" data-ng-show=\"sending\"><i class=\"fa fa-cog fa-spin fa-5x\" aria-hidden=\"true\"></i></div>\n" +
+    "\n" +
+    "    <div class=\"alert alert-success\" role=\"alert\" data-ng-show=\"thankyou\">\n" +
+    "        <p><em><strong>Thank you</strong> for contacting us.</em> Our team will be in touch soon.</p>\n" +
+    "    </div>\n" +
     "</div>\n" +
     "\n" +
     "");
@@ -207,12 +236,14 @@ angular.module("passion/passion.tpl.html", []).run(["$templateCache", function($
     "    <!-- h2>Building Platforms</h2>\n" +
     "    <p></p -->\n" +
     "    <h2>Solution Development</h2>\n" +
-    "    <p>Need to get to market quickly and cheaply? The we can help you achieve this by working with you to determine the best approach and selecting the best third party software that will get your ideas or business to a place that allows you to promote, market and sell.</p>\n" +
+    "    <p>Need to get to market quickly and cheaply? The we can help you achieve this!</p>\n" +
+    "    <p>We working with you to determine the best approach and by selecting the most appropriate third party software that will get your ideas or business to a place that allows you to promote, market and sell.</p>\n" +
     "    <h2>Services Integration</h2>\n" +
-    "    <p>We have specialist in using software and infrastructure created by others and putting them together to create the IT Systems that your business needs to enable you to save money and time.</p>\n" +
-    "    <p>We have specialist that create desktop applications, mobile applications, web sites and web applications using the appropriate technology for the platform that the software is being distributed too.</p>\n" +
+    "    <p>We have specialied in using software and infrastructure created by others and putting them together to create the IT Systems that your business needs to enable you to save money and time.</p>\n" +
+    "    <p>Our specialist team creates desktop applications, mobile applications, web sites and web applications using the appropriate technology for the platform that the software is being distributed too.</p>\n" +
+    "    <br/>\n" +
     "    <blockquote>\n" +
-    "        <p class=\"quote\">We are people with a passion for technology aiming to achieve a fast and excellence technical response that adds value to a business.</p>\n" +
+    "        <p class=\"text-center\">We are people with a passion for technology aiming to achieve a fast and excellence technical response that adds value to a business.</p>\n" +
     "    </blockquote>\n" +
     "</div>\n" +
     "\n" +
